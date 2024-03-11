c99706b71c75a631cbe280baefb5cc0ecbf31e58
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2024-03-11 20:45:18 +0530

    added linked list

--- main.py
+++ main.py
@@ -24,6 +24,44 @@ class LinkedList:
                 temp = temp.next
             temp.next = node
 
+    def pop(self):
+        if self.head == None:
+            return print("List is Empty")
+        else:
+            if self.head.next == None:
+                self.head = None
+            else:
+                temp = self.head
+                while temp and temp.next.next != None:
+                    temp = temp.next
+                temp.next = None
+
+    def deleteElement(self, data):
+        if self.head == None:
+            return print("List is Empty")
+        else:
+            if self.head.val == data:
+                self.head = self.head.next
+            else:
+                temp = self.head
+                while temp and temp.next and temp.next.val != data:
+                    temp = temp.next
+                if temp.next and temp.next.val != data:
+                    temp.next = temp.next.next
+                else:
+                    print("Element Not Found")
+
+    def size(self):
+        if self.head == None:
+            print(0)
+        else:
+            total = 0
+            temp = self.head
+            while temp:
+                total += 1
+                temp = temp.next
+            print(total)
+
 
 list = LinkedList()
 list.printList()
@@ -31,4 +69,10 @@ list.append(10)
 list.append(20)
 list.append(30)
 list.append(40)
+
+list.deleteElement(50)
+
 list.printList()
+list.append(40)
+list.pop()
+list.size()
